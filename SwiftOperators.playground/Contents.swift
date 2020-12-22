0e94159cfde91383d9d58462dec66d0f677224fa
ankur-manikandan <ankursg8@gmail.com>
2020-12-22 11:37:11 -0800

    swift operators

---
+++ SwiftOperators.playground/Contents.swift
@@ -0,0 +1,29 @@
+// Unary operators
+print(!true)
+var a = -5
+print(a)
+
+// Binary operators
+var result = 10 + 30
+print(result)
+
+// Logical operators
+print(true && true)
+print(true && false)
+print(true || false)
+
+// Ternary operators
+// condition ? value 1 : value 2
+let bool_result = (5 > 134) ? "Larger value" : "Smaller value"
+print(bool_result)
+
+// Bitwise operators
+var x = 0
+var y = 1
+print(x & y)
+
+/*
+ If you use two bitwise AND or OR operators like below, you will get an error.
+ Two bitwise operators are only applicable to boolean values.
+ print(true & false)
+ */
